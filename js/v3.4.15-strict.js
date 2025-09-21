94afc01ca5e8457bdf5c7540aa33f4296d9cbbf1
ChetanGohil2829 <chetan.gohil28@gmail.com>
2025-09-21 23:14:10 +0100

    updated code

--- js/v3.4.15-strict.js
+++
@@ -1,8 +0,0 @@
-
-// Ensure autoplay for music
-document.addEventListener('DOMContentLoaded', function() {
-  const audioElements = document.querySelectorAll('audio');
-  audioElements.forEach(audio => {
-    audio.play();
-  });
-});
